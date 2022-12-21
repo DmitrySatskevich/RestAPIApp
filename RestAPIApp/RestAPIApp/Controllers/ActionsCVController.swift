758be74652f749a71ad1931f9309198e88bdae66
RecrutSatsckevich <100848161+DmitrySatskevich@users.noreply.github.com>
2022-12-21 15:29:57 +0300

    UICollectionVC - the logic is fulfilled

---
+++ RestAPIApp/RestAPIApp/Controllers/ActionsCVController.swift
@@ -0,0 +1,87 @@
+//
+//  ActionsCVController.swift
+//  RestAPIApp
+//
+//  Created by dzmitry on 21.12.22.
+//
+
+import UIKit
+
+// CaseIterable позволяет из перечисления сделать массив
+enum UserActions: String, CaseIterable {
+    case downloadImage = "Download Image"
+    case users = "Users"
+}
+
+class ActionsCVController: UICollectionViewController {
+    
+    private let reuseIdentifier = "Cell"
+    private let userActions = UserActions.allCases
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // очищает ячейку, при возврате на этот ActionsCVController
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Здесь привязывается XIB к ActionsCVController
+        // Register cell classes
+        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    // количество секций
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        userActions.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? ActionCVCell else { return UICollectionViewCell() }
+        cell.infoLbl.text = userActions[indexPath.row].rawValue
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+}
+
+extension ActionsCVController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: (UIScreen.main.bounds.width), height: 80)
+    }
+}
